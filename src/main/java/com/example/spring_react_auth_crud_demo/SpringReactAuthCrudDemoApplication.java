2ab6d9206b3c6ad8669debb449058e7544fa04fe
BorisShek <Boris.Shek@haw-hamburg.de>
2022-08-12 23:19:27 +0200

    first commit

---
+++ src/main/java/com/example/spring_react_auth_crud_demo/SpringReactAuthCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_react_auth_crud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactAuthCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactAuthCrudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_react_auth_crud_demo/SpringReactAuthCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_react_auth_crud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAuthCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
